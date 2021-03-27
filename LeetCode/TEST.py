c232bd230d62313d272db3a1f07c08b8fb214f29
TOOFACK <paulrussianvodka@gmail.com>
2021-03-27 15:56:44 +0300

    Medium O(NLogN), not my sol,

--- LeetCode/TEST.py
+++ LeetCode/TEST.py
@@ -1,18 +1,4 @@
 import collections
 from collections import deque
 
-lst = [1,2,3]
-print(lst[0:1])
-print(lst)
-b = [17,13,11,2,3,5,7]
-list.sort(b)
-ans = collections.deque(b)
-print(ans)
-ans.rotate(-1)
-print(ans)
-ans.rotate(2)
-print(ans)
-ans.rotate(3)
-print(ans)
-ans.rotate(4)
-print(ans)
+print(1//2)
