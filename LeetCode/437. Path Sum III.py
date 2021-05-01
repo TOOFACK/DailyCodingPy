7c33b2f5a1fd2d16ea219217e6aa57f2e37c675b
TOOFACK <paulrussianvodka@gmail.com>
2021-05-01 11:54:05 +0300

    not 1 O(n*n), 2 O(n) Medium

--- LeetCode/437. Path Sum III.py
+++ LeetCode/437. Path Sum III.py
@@ -5,9 +5,50 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    def __init__(self):
+        self.ans = 0
+
     def pathSum(self, root, targetSum):
         """
         :type root: TreeNode
         :type targetSum: int
         :rtype: int
         """
+
+        # def helper(root, sum_p):
+        #     if not root:
+        #         return 0
+        #     if root.val == sum_p:
+        #         return 1 + helper(root.left, sum_p - root.val) + helper(root.right, sum_p - root.val)
+        #     else:
+        #         return helper(root.left, sum_p - root.val) + helper(root.right, sum_p - root.val)
+        #
+        # if not root:
+        #     return 0
+        # return self.pathSum(root.left, targetSum) + self.pathSum(root.right, targetSum) + helper(root, targetSum)
+        #
+        #
+
+        summary = {0: 1}
+
+        def helper(root, r_sum, t):
+            if not root:
+                return 0
+            r_sum += root.val
+
+            if r_sum - t in summary:
+                cnt = summary[r_sum - t]
+
+            else:
+                cnt = 0
+
+            if r_sum in summary:
+                summary[r_sum] = summary[r_sum] + 1
+            else:
+                summary[r_sum] = 1
+
+            cnt += helper(root.left, r_sum, t) + helper(root.right, r_sum, t)
+            summary[r_sum] = summary[r_sum] - 1
+            return cnt
+
+        return helper(root, 0, targetSum)
