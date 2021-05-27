ca9f40e719a3a1811431546880497781b2b916aa
TOOFACK <paulrussianvodka@gmail.com>
2021-05-27 19:58:41 +0300

    not O(N)  my Medium

---
+++ LeetCode/141. Linked List Cycle.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        fast = head
+        slow = head
+        tr = False
+
+        while True:
+
+            slow = slow.next
+
+            if fast.next:
+                fast = fast.next.next
+            else:
+                return False
+
+            if not(slow or fast):
+                return False
+
+            if slow == fast:
+                return True
+
