025ce9571e37c6adad9a8f800af947f127aa2b18
TOOFACK <paulrussianvodka@gmail.com>
2021-06-16 21:12:43 +0300

    gotta work to do

--- LeetCode/146. LRU Cache.py
+++ LeetCode/146. LRU Cache.py
@@ -1,4 +1,3 @@
-
 class Node():
     def __init__(self, k, v):
         self.key = k
@@ -21,20 +20,20 @@ class LRUCache(object):
         self.head.next = self.tail
         self.tail.prev = self.head
 
-
-
-
-
     def get(self, key):
         """
         :type key: int
         :rtype: int
         """
         if key in self.LRU:
-            return self.LRU[key].val
+            n = self.LRU[key]
+            self._remove(n)
+            self._add(n)
+
+
+            return n.val
         else:
             return -1
-
 
     def put(self, key, value):
         """
@@ -42,12 +41,28 @@ class LRUCache(object):
         :type value: int
         :rtype: None
         """
+        if key in self.LRU:
+            self._remove(self.LRU[key])
+        n = Node(key, value)
+        self._add(n)
+        self.LRU[key] = n
+        if len(self.LRU) > self.max_cap:
+            n = self.head.next
+            self._remove(n)
+            del self.LRU[n.key]
 
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
 
-
-
-
-
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node
+        node.prev = p
+        node.next = self.tail
 
 
 # Your LRUCache object will be instantiated and called as such:
